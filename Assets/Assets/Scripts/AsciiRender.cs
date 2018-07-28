6726008d3cfed729b1b3d6882c328aaecd565719
Knar33 <adbyerly@comcast.net>
2018-07-28 18:46:42 -0500

    Added variable screensize

--- Assets/Assets/Scripts/AsciiRender.cs
+++ Assets/Assets/Scripts/AsciiRender.cs
@@ -12,12 +12,16 @@ public class AsciiRender : MonoBehaviour
     public TextMeshProUGUI renderText;
     private int charWidth;
     private int charHeight;
+    private int arraySize;
     private char[] asciiCharArray;
     private char[] greyscaleAscii = new char[] { '$', '@', 'B', '%', '8', '&', 'W', 'M', '#', '*', 'o', 'a', 'h', 'k', 'b', 'd', 'p', 'q', 'w', 'm', 'Z', 'O', '0', 'Q', 'L', 'C', 'J', 'U', 'Y', 'X', 'z', 'c', 'v', 'u', 'n', 'x', 'r', 'j', 'f', 't', '/', '\\', '|', '(', ')', '1', '{', '}', '[', ']', '?', '-', '_', '+', '~', '<', '>', 'i', '!', 'l', 'I', ';', ':', ',', '"', '^', '`', '\'', '.', ' ' };
 
     private void Start()
     {
-        asciiCharArray = new char[57814];
+        charHeight = Screen.height / 6;
+        charWidth = Screen.width / 6;
+        arraySize = ((charWidth + 1) * charHeight) + 34;
+        asciiCharArray = new char[arraySize];
         asciiCharArray[0] = '<';
         asciiCharArray[1] = 'm';
         asciiCharArray[2] = 's';
@@ -43,15 +47,15 @@ public class AsciiRender : MonoBehaviour
         asciiCharArray[22] = '=';
         asciiCharArray[23] = '6';
         asciiCharArray[24] = '>';
-        asciiCharArray[57805] = '<';
-        asciiCharArray[57806] = '/';
-        asciiCharArray[57807] = 'm';
-        asciiCharArray[57808] = 's';
-        asciiCharArray[57809] = 'p';
-        asciiCharArray[57810] = 'a';
-        asciiCharArray[57811] = 'c';
-        asciiCharArray[57812] = 'e';
-        asciiCharArray[57813] = '>';
+        asciiCharArray[arraySize - 9] = '<';
+        asciiCharArray[arraySize - 8] = '/';
+        asciiCharArray[arraySize - 7] = 'm';
+        asciiCharArray[arraySize - 6] = 's';
+        asciiCharArray[arraySize - 5] = 'p';
+        asciiCharArray[arraySize - 4] = 'a';
+        asciiCharArray[arraySize - 3] = 'c';
+        asciiCharArray[arraySize - 2] = 'e';
+        asciiCharArray[arraySize - 1] = '>';
     }
 
     void Update()
@@ -61,16 +65,16 @@ public class AsciiRender : MonoBehaviour
         tex2d.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
         tex2d.Apply();
 
-        Color[] renderGrid = tex2d.GetPixels(0, 0, 320, 180);
+        Color[] renderGrid = tex2d.GetPixels(0, 0, charWidth, charHeight);
 
-        for (int y = 0; y < 180; y++)
+        for (int y = 0; y < charHeight; y++)
         {
-            for (int x = 0; x < 320; x++)
+            for (int x = 0; x < charWidth; x++)
             {
-                int charSpace = 25 + (1 * y) + x + (320 * y);
-                asciiCharArray[charSpace] = getGreyscaleChar(Convert.ToDouble(renderGrid[x + (320 * (179 - y))].grayscale));
+                int charSpace = 25 + (1 * y) + x + (charWidth * y);
+                asciiCharArray[charSpace] = getGreyscaleChar(Convert.ToDouble(renderGrid[x + (charWidth * (charHeight - 1 - y))].grayscale));
             }
-            asciiCharArray[25 + 320 + (321 * y)] = '\n';
+            asciiCharArray[25 + charWidth + ((charWidth + 1) * y)] = '\n';
         }
         string output = new string(asciiCharArray);
         renderText.text = output;
@@ -78,7 +82,6 @@ public class AsciiRender : MonoBehaviour
 
     public char getGreyscaleChar(double hue)
     {
-        int charPos = Convert.ToInt32(Math.Ceiling(hue * (greyscaleAscii.Length - 1)));
-        return greyscaleAscii[(greyscaleAscii.Length - 1) - charPos];
+        return greyscaleAscii[(greyscaleAscii.Length - 1) - Convert.ToInt32(Math.Ceiling(hue * (greyscaleAscii.Length - 1)))];
     }
 }
